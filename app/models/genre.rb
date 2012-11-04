78aee4a0da2b6f8f309d20b866755cd9ed8ddcbf
Janis <janisk@ithouse.lv>
2012-11-04 20:26:30 +0200

    depended added to has_many associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ActiveRecord::Base
   validates_presence_of :name
-  has_many :albums
+  has_many :albums, dependent: :destroy
   attr_accessible :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  has_many :albums
+  has_many :albums, dependent: :destroy
 end
